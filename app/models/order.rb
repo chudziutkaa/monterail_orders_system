0c4d2cfb7da291505e374b2f9b36a70d3917c552
chudziutkaa <klaudiachudy94@gmail.com>
2016-06-01 11:41:14 +0200

    Add Meal model and validates association between Meal and Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,9 @@
 class Order < ActiveRecord::Base
   validates :restaurant_name, presence: true
+  has_many :meals
+
+  def as_json(options = {})
+    super(options.merge(include: :meals))
+  end
+
 end
