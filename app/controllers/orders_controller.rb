d449c646cc08adc6ea20f09ef63af5cdfe7ed19f
chudziutkaa <klaudiachudy94@gmail.com>
2016-06-01 11:42:51 +0200

    Add #index, #create and #show action to OrdersController + strong params

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,3 +1,19 @@
 class OrdersController < ApplicationController
+  def index
+    respond_with Order.all
+  end
+
+  def create
+    respond_with Order.create(order_params)
+  end
+
+  def show
+    respond_with Order.find(params[:id])
+  end
   
+  private
+
+  def order_params
+    params.require(:order).permit(:restaurant_name)
+  end
 end
